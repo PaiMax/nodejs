fb2bb279448f76045401aef90156f854b00f5395
PaiMax <nishwalbh1997@gmail.com>
2023-07-20 13:48:20 +0530

    sending response from the server in the form html

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log('nishwal bhandari');
+    if(req.url==='/home'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body> <h1>Welcome home</h1></body>');
+        res.write('</html>');
+        
+
+
+    }
+    if(req.url==='/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body> <h1> Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        
+    }
+    if(req.url==='/node'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body> <h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        
+    }
+    res.end();
+    
+
+   
+    
 });
-server.listen(4000);
+server.listen(8000);
