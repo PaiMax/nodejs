ce2aca4d702596ae2ce8212c32fb1019ab250a59
PaiMax <nishwalbh1997@gmail.com>
2023-07-20 18:21:45 +0530

    Writing and Reading from file

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const http=require('http');
+const fs=require('fs');
+const { buffer } = require('stream/consumers');
 const server=http.createServer((req,res)=>{
-    if(req.url==='/home'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<body> <h1>Welcome home</h1></body>');
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+        
+        fs.readFile('message.txt','utf-8',(err,data)=>{
+            res.setHeader('Content-Type','text/html');
+            res.write('<html>');
+        res.write(`<body> <p>${data}</p><br><form action ="/message" method="POST" ><input type="text" name="message"><button type="submit">Send</button></form></body>`);
         res.write('</html>');
+       
+        
+        return res.end();
+        
+            
+            
+        })
+        
+        
         
 
 
     }
-    if(req.url==='/about'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<body> <h1> Welcome to About Us page</h1></body>');
-        res.write('</html>');
-        
-    }
-    if(req.url==='/node'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<body> <h1>Welcome to my Node Js project</h1></body>');
-        res.write('</html>');
-        
-    }
-    res.end();
-    
+    if(url==='/message'&& method==='POST'){
+        const body=[];
+        req.on('data',(chunk)=> {
+            body.push(chunk);
+        });
+        req.on('end',()=>{
 
-   
+            const parsedBody=Buffer.concat(body).toString();
+            const message=parsedBody.split('=')[1];
+            console.log(message);
+            fs.writeFile('message.txt',message,(err)=>{
+                res.statusCode=302;
+                res.setHeader('Location','/');
+                return res.end();});
+
+            
+            
+        });
+    }
     
 });
+    
+   
 server.listen(8000);
