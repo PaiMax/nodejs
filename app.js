a2779cec97f2ae6c9bb016def67de6ede1b282f9
PaiMax <nishwalbh1997@gmail.com>
2023-07-21 13:35:02 +0530

    express js used

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const http=require('http');
-const routes=require('./routes');
+//const http=require('http');
+//const routes=require('./routes');
+const express=require('express');
+const app=express();
 
-console.log(routes.someText);
-const server=http.createServer(routes.handler);
+app.use((req,res,next)=>{  
+    console.log('middleware 1');   
+    next();                                   
+
+})//allow us to add new middleware function
+
+app.use((req,res,next)=>{  
+    console.log('middleware 2');
+    res.send('<h1>hello express</h1>');                                 
+
+})
+
+
+//const server=http.createServer(app);
     
    
-server.listen(8000);
+//server.listen(8000);
+app.listen(8000);
