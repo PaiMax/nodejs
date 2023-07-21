6a745478f2b821bfb1e7d4700ecd734657fdb3cc
PaiMax <nishwalbh1997@gmail.com>
2023-07-21 19:18:50 +0530

    filtering request which has comman starting path

--- app.js
+++ app.js
@@ -2,29 +2,20 @@
 //const routes=require('./routes');
 const express=require('express');
 const app=express();
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 const bodyparser=require('body-parser');
 app.use(bodyparser.urlencoded({extended: false}));
-app.use('/add-product',(req,res,next)=>{  
-     res.send('<form action="/product" method="POST"> <input type="text" name="title"><input type="number" name="product-size"><button type="submit">Add Product</button></form>');                                 
-
-})
-
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>');
 })
 
 
-app.use('/',(req,res,next)=>{  
-   next();                                   
 
-})//allow us to add new middleware function
 
-app.use((req,res,next)=>{  
-    
-    res.send('<h1>hello express</h1>');                                 
 
-})
 
 
 //const server=http.createServer(app);
